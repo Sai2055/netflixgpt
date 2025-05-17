26dbfb99a741c8f03f7a0634b900b493732c9d4a
Sai Sajja <sajjasai7746@gmail.com>
2025-05-17 09:45:29 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
+import Login from './Components/Login'
 
 function App() {
   
   return (
     <>
-      <div className='flex items-center justify-center h-screen w-full'>
-        <p className='text-3xl text-center'>Hello Sai Sajja!</p>
+      <div>
+        <Login/>
       </div>
     </>
   )
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gradient-to-b from-black to-transperant h-20 w-full absolute z-10' >
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" className=' w-30 my-6 mx-12 absolute'/>
+    </div>
+  )
+}
+
+export default Header
